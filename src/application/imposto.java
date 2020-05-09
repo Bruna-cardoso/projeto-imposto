fd51fdd76ea3bb6cc8326dc6ee977cd806a38a17
Bruna Cardoso <bruna.carolinepc@gmail.com>
2020-05-09 00:05:44 -0300

    Leitura dos dados

--- src/application/imposto.java
+++ src/application/imposto.java
@@ -1,9 +1,34 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class imposto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double impostosalario, impostoservicos, impostocapital, salario, salariomensal, servicos, capital, gastosm, gastose;
+		
+		// Leitura dos dados
+		System.out.println("Renda anual com salário:");
+		salario = sc.nextDouble();
+		
+		System.out.println("Renda anual com prestação de serviço:");
+		servicos = sc.nextDouble();
+		
+		System.out.println("Renda anual com ganho de capital:");
+		capital = sc.nextDouble();
+		
+		System.out.println("Gastos médicos:");
+		gastosm = sc.nextDouble();
+		
+		System.out.println("Gastos educacionais:");
+		gastose = sc.nextDouble();
+		
+		sc.close();
 
 	}
 
